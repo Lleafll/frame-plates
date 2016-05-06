0d9214bfec34dfb3e700781ee3f3ab2ac2effe23
Lars Lorenz <lls-bin@web.de>
2016-05-06 23:38:02 +0200

    Initial commit

---
+++ FramePlates.lua
@@ -0,0 +1,26 @@
+--------------
+-- Settings --
+--------------
+local frameCount = 10
+local rows = 2
+
+
+--------------
+-- Upvalues --
+--------------
+
+
+---------------
+-- Constants --
+---------------
+
+
+---------------
+-- Variables --
+---------------
+local columns = math.ceil(frameCount/rows)
+
+
+---------------
+-- Functions --
+---------------
