01023eaa904c1d3663f76091dbc895b3eca1acd6
Lleafll <geobugi@web.de>
2016-08-16 00:04:55 +0200

    Handle movement of frames (only out of combat)

--- EffigyFrame.lua
+++ EffigyFrame.lua
@@ -16,7 +16,6 @@ local UI_SCALE = UIParent:GetScale()
 -- Settings --
 --------------
 local frameCount = 15
-local growthDirection = "VERTICAL"  -- VERTICAL, HORIZONTAL
 local frameWidth = 163 / UI_SCALE
 local frameHeight = 30 / UI_SCALE
 local framePadding = 2
@@ -92,18 +91,6 @@ local borderBackdrop = {
 
 
 
-------------
--- Lookup --
-------------
-local oppositePoint = {
-  RIGHT = "LEFT",
-  DOWN = "UP",
-  RIGHT = "LEFT",
-  UP = "DOWN"
-}
-
-
-
 ------------------
 -- Parent Frame --
 ------------------
@@ -113,13 +100,8 @@ EffigyFrameParent.background:SetAllPoints()
 EffigyFrameParent.background:SetBackdrop(backdrop)
 EffigyFrameParent.background:SetBackdropColor(0.5, 0.5, 0.5, 0.5)
 EffigyFrameParent.background:Hide()
-if growthDirection == "VERTICAL" then
-  EffigyFrameParent:SetWidth(UI_SCALE * frameWidth)
-  EffigyFrameParent:SetHeight(UI_SCALE * (frameHeight * frameCount + framePadding * (frameCount - 1)))
-else
-  EffigyFrameParent:SetWidth(UI_SCALE * (frameWidth * frameCount + framePadding * (frameCount - 1)))
-  EffigyFrameParent:SetHeight(UI_SCALE * frameHeight)
-end -- if
+EffigyFrameParent:SetWidth(UI_SCALE * frameWidth)
+EffigyFrameParent:SetHeight(UI_SCALE * frameHeight)
 EffigyFrameParent:SetMovable(true)
 EffigyFrameParent:SetUserPlaced(false)
 EffigyFrameParent:Show()
@@ -129,12 +111,7 @@ EffigyFrameParent:Show()
 ---------------
 -- Functions --
 ---------------
-do  
-  local function getHealthBar(unitID)
-    local nameplateUnitFrame = C_NamePlate_GetNamePlateForUnit(unitID).UnitFrame
-    return nameplateUnitFrame.healthBar or nameplateUnitFrame.HealthBar
-  end -- function
-  
+do
   local function eventHandler(self, event, unitID)
     if event == "UNIT_HEALTH_FREQUENT" then
       self.statusbar:SetValue(UnitHealth(unitID))
@@ -145,14 +122,26 @@ do
     elseif event == "NAME_PLATE_UNIT_ADDED" then
       self.statusbar:SetMinMaxValues(0, UnitHealthMax(unitID))
       self.statusbar:SetValue(UnitHealth(unitID))
-      self.healthBar = getHealthBar(unitID)
       self.NameFontString:SetText(UnitName(unitID))
-    end -- if
-  end -- function
+      if UnitName(unitID) == "Soul Effigy" then
+        self.parent:SetPoint("BOTTOMLEFT")
+      end
+    elseif event == "NAME_PLATE_UNIT_REMOVED" then
+      self.parent:SetPoint("BOTTOMLEFT", UIParent, "TOPRIGHT")
+    end
+  end
 
-  function EffigyFrameParent:CreateEffigyFrame(unitID, posX, posY)
+  function EffigyFrameParent:CreateEffigyFrame(unitID)
+    -- Parent frame (for position manipulation)
+    local parentFrame = CreateFrame("Frame", "$parent".."_"..unitID, self)
+    parentFrame:SetWidth(UI_SCALE * frameWidth)
+    parentFrame:SetHeight(UI_SCALE * frameHeight)
+    parentFrame:SetPoint("BOTTOMLEFT", UIParent, "TOPRIGHT")
+    parentFrame:Show()
+    
     -- Secure frame
-    local frame = CreateFrame("BUTTON", "$parent".."_"..unitID, self, "SecureUnitButtonTemplate")
+    local frame = CreateFrame("BUTTON", "$parentButton", parentFrame, "SecureUnitButtonTemplate")
+    frame.parent = parentFrame
     self[unitID] = frame
     frame.unitID = unitID
     frame:SetAttribute("type", "target")
@@ -161,7 +150,7 @@ do
     RegisterUnitWatch(frame)
     frame:SetWidth(UI_SCALE * frameWidth)
     frame:SetHeight(UI_SCALE * frameHeight)
-    frame:SetPoint("BOTTOMLEFT", UI_SCALE * posX, UI_SCALE * posY)
+    frame:SetPoint("BOTTOMLEFT")
     frame:Show()
     
     -- Background
@@ -193,7 +182,7 @@ do
     frame.NameFontString:SetText(UnitName(unitID))
     if fontShadow then
       frame.NameFontString:SetShadowOffset(1, -1)
-    end -- if
+    end
     
     -- Highlighted
     frame.highlight = frame:CreateTexture(nil, "HIGHLIGHT")
@@ -214,6 +203,7 @@ do
     frame:RegisterUnitEvent("UNIT_MAXHEALTH", unitID)
     frame:RegisterUnitEvent("UNIT_NAME_UPDATE", unitID)
     frame:RegisterUnitEvent("NAME_PLATE_UNIT_ADDED", unitID)
+    frame:RegisterUnitEvent("NAME_PLATE_UNIT_REMOVED", unitID)
     frame:SetScript("OnEvent", eventHandler)
     
     -- Frame levels
@@ -222,8 +212,8 @@ do
     frame.statusbar:SetFrameStrata("BACKGROUND")
     
     return frame
-  end -- function
-end -- do
+  end
+end
 
 
 
@@ -231,23 +221,10 @@ end -- do
 -- Frame Creation --
 --------------------
 function EffigyFrameParent:CreateFrames()
-  local i = 1
-  local posX = 0
-  local posY = 0
-  if growthDirection == "VERTICAL" then
-    local posYIncrement = (frameHeight + framePadding) * (reverseY and - 1 or 1) + 2  -- +2 for border
-    for i = 1, frameCount do
-      EffigyFrameParent:CreateEffigyFrame("nameplate"..i, posX, posY)
-      posY = posY + posYIncrement
-    end -- for
-  else
-    local posXIncrement = (frameWidth + framePadding) * (reverseX and - 1 or 1) + 2  -- +2 for border
-    for i = 1, frameCount do
-      EffigyFrameParent:CreateEffigyFrame("nameplate"..i, posX, posY)
-      posX = posX + posXIncrement
-    end -- for
-  end -- if
-end -- function
+  for i = 1, frameCount do
+    self:CreateEffigyFrame("nameplate"..i)
+  end
+end
 
 
 
@@ -264,7 +241,7 @@ do
     db.posY = math_floor(posY / UI_SCALE + 0.5)
     
     self:SetPoint("BOTTOMLEFT", UI_SCALE * db.posX, UI_SCALE * db.posY)
-  end -- function
+  end
 
   function EffigyFrameParent:Unlock()
     self.background:Show()
@@ -275,22 +252,22 @@ do
         self:Lock()
       elseif button == "LeftButton" then
         self:StartMoving()
-      end -- if
-    end) -- function
+      end
+    end)
     self:SetScript("OnMouseUp", function(self, button)
       dragStop(self)
-    end) -- function
+    end)
     self:SetScript("OnMouseWheel", function(self, delta)
       if IsShiftKeyDown() then
         db.posX = db.posX + delta / UI_SCALE
       else
         db.posY = db.posY + delta / UI_SCALE
-      end -- if
+      end
       self:SetPoint("BOTTOMLEFT", UI_SCALE * db.posX, UI_SCALE * db.posY)
-    end) -- function
+    end)
     self.unlocked = true
     print("EffigyFrame unlocked")
-  end -- function
+  end
 
   function EffigyFrameParent:Lock()
     self.background:Hide()
@@ -300,8 +277,8 @@ do
     self:SetScript("OnMouseWheel", nil)
     self.unlocked = false
     print("EffigyFrame locked")
-  end -- function
-end -- do
+  end
+end
 
 
 
@@ -318,11 +295,11 @@ do
       self:SetPoint("BOTTOMLEFT", UI_SCALE * db.posX, UI_SCALE * db.posY)
       self:UnregisterEvent("ADDON_LOADED")
       self:CreateFrames()
-    end -- if
-  end -- function
+    end
+  end 
   EffigyFrameParent:RegisterEvent("ADDON_LOADED")
   EffigyFrameParent:SetScript("OnEvent", eventHandler)
-end -- do
+end
 
 
 
@@ -336,5 +313,5 @@ SlashCmdList.EFFIGYFRAME = function()
     EffigyFrameParent:Lock()
   else
     EffigyFrameParent:Unlock()
-  end -- if
-end -- function
+  end
+end
